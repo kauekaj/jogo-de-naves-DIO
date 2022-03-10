62d712110097338236cf117ac9a1153fd3612ed3
Kauê Jacyntho <kauekaj@gmail.com>
2022-03-10 18:36:03 -0300

    create function shoot

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ function start() { // Inicio da função start()
 
     //Principais variáveis do jogo
 	
+    var podeAtirar=true;
 	var jogo = {}
     var velocidade=5;
     var posicaoY = parseInt(Math.random() * 334);
 	var TECLA = {
         T: 84,
         G: 71,
-        Space: 8
+        Space: 32
         }
     
         
@@ -83,6 +84,7 @@ function start() { // Inicio da função start()
             if (jogo.pressionou[TECLA.Space]) {
                 
                 //Chama função Disparo	
+                disparo();
             }
         
             } // fim da função movejogador()
@@ -124,5 +126,38 @@ function start() { // Inicio da função start()
                 }
         
         } // fim da função moveamigo()
+
+        function disparo() {
+	
+            if (podeAtirar==true) {
+                
+            podeAtirar=false;
+            
+            topo = parseInt($("#jogador").css("top"))
+            posicaoX= parseInt($("#jogador").css("left"))
+            tiroX = posicaoX + 190;
+            topoTiro=topo+37;
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top",topoTiro);
+            $("#disparo").css("left",tiroX);
+            
+            var tempoDisparo=window.setInterval(executaDisparo, 30);
+            
+            } //Fecha podeAtirar
+         
+                   function executaDisparo() {
+                posicaoX = parseInt($("#disparo").css("left"));
+                $("#disparo").css("left",posicaoX+15); 
+        
+                        if (posicaoX>900) {
+                                
+                    window.clearInterval(tempoDisparo);
+                    tempoDisparo=null;
+                    $("#disparo").remove();
+                    podeAtirar=true;
+                            
+                           }
+            } // Fecha executaDisparo()
+        } // Fecha disparo()
 
 } // Fim da função start
